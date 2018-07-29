371b596761e7aed9c22f953825853570cd3a3fea
Nic Wilson <nicholaswilson3010@gmail.com>
2018-07-29 16:51:21 +0100

    grid and snake rendered, attempting refactor into constructor for grid

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
-console.log('loaded');
-
 $(document).ready(init);
+const gridIndex = [];
+let facing = 'right';
+let currentPosition = 190;
+let bodyPosition = [186, 187, 188, 189];
+const movements = {
+  'up':
+}
 
 function init() {
   const $grid = $('.grid');
-  const gridIndex = [];
-
   for (var i = 0; i < 400; i++) {
     gridIndex.push(i);
   }
@@ -14,11 +17,35 @@ function init() {
     $grid.append($('<div></div>'));
   });
 
-  $grid.children().toArray().forEach((cell) => {
+  $grid.children().toArray().forEach((cell, i) => {
+    $(cell).addClass(`${i}`);
+    $(cell).hasClass(`${currentPosition}`) ? $(cell).addClass('head') : $(cell).removeClass('head');
+    bodyPosition.includes(i) ? $(cell).addClass('body') : $(cell).removeClass('body');
     $(cell).css({
       height: '40px',
       width: '40px'
     });
   });
+
+  $(window).on('keydown', (e) => {
+    switch(e.keyCode){
+      case 37:
+        facing = 'left';
+        break;
+      case 38:
+        facing = 'up';
+        break;
+      case 39:
+        facing = 'right';
+        break;
+      case 40:
+        facing = 'down';
+        break;
+    }
+  });
+
+  // function movement() {
+  //
+  // }
 
 }
